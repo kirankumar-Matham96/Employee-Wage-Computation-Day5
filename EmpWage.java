37da7fc9d798c7183856bc26244028bb85378cfd
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-03-24 12:04:12 +0530

    ADD: Checking Employee Presence

--- EmpWage.java
+++ EmpWage.java
@@ -2,6 +2,15 @@ public class EmpWage
 {
 	public static void main(String[] args)
 	{
-			System.out.println("Welcome to Employee Wage Computation Program!");
+		int isPresent = 1;
+		int checkPresence = (int) Math.floor(Math.random()*10%2);
+		if(isPresent == checkPresence)
+		{
+			System.out.println("Employee is present");
+		}
+		else
+		{
+			System.out.println("Employee is absent");
+		}
 	}
 }
