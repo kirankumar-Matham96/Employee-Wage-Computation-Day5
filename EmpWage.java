ae3ea123980ff11ca27f186b67a7f962276a3165
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-03-24 12:17:02 +0530

    REFRACTED: Usede switch case

--- EmpWage.java
+++ EmpWage.java
@@ -2,31 +2,31 @@ public class EmpWage
 {
 	public static void main(String[] args)
 	{
-		int isPresent = 1;
-		int isFullTime = 1;
+		final int isPresent = 1;
+		final int isFullTime = 1;
 		int wagePerHr = 20;
 		int fullTimeWorkingHrs = 8;
 		int partTimeWorkingHrs = 4;
 		int empWage = 0;
 		int checkPresence = (int) (Math.random()*10%2);
 		int checkWorkingTime = (int) (Math.random()*10%2);
-		if(isPresent == checkPresence)
+		switch(checkPresence)
 		{
-			System.out.println("Employee is present");
-			if(isFullTime == checkWorkingTime)
-			{
-				System.out.println("Employee is full timer");
-				empWage = fullTimeWorkingHrs * wagePerHr;
-			}
-			else
-			{
-				System.out.println("Employee is part timer");
-				empWage = partTimeWorkingHrs * wagePerHr;
-			}
-		}
-		else
-		{
-			System.out.println("Employee is absent");
+			case isPresent:
+				System.out.println("Employee is present");
+				switch(checkWorkingTime)
+				{
+					case isFullTime:
+						System.out.println("Employee is full timer");
+						empWage = fullTimeWorkingHrs * wagePerHr;
+						break;
+					default:
+						System.out.println("Employee is part timer");
+						empWage = partTimeWorkingHrs * wagePerHr;
+				}
+				break;
+			default:
+				System.out.println("Employee is absent");
 		}
 		System.out.println("Employee Wage is: "+empWage);
 	}
